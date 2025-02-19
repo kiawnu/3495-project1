63e5a1c9b9bcbf6726128610abfc6f1644549a54
Kian Abdollahi <kabdollahi347@gmail.com>
2025-02-19 10:49:21 -0800

    analyzer service working

--- analytics/app.py
+++ analytics/app.py
@@ -1,26 +1,48 @@
 # TODO:
 # 1. connect to mongodb and dump the stats
 #
-# 2. Set it up to run on some frequency 
+# 2. Set it up to run on some frequency
 #   -> reference tims lab?
 
 import mysql.connector
-# import pymongo
+
+from pymongo import MongoClient
 import yaml
 import time
 
-with open('app_conf.yml', 'r') as f:
+with open("app_conf.yml", "r") as f:
     CONFIG = yaml.safe_load(f.read())
 
-#time.sleep(5)
+MONGO_URI = CONFIG["mongo"]["uri"]
+DB_NAME = CONFIG["mongo"]["database"]
+
+
+# time.sleep(5)
 def establish_sql_connection():
     return mysql.connector.connect(
-        host="127.0.0.1",
-        user=CONFIG['mysql']['user'],
-        password=CONFIG['mysql']['user_password'],
-        database=CONFIG['mysql']['database'],
-        port=CONFIG['mysql']['port']
+        host="mysql",
+        user=CONFIG["mysql"]["user"],
+        password=CONFIG["mysql"]["user_password"],
+        database=CONFIG["mysql"]["database"],
+        port=CONFIG["mysql"]["port"],
     )
+
+
+def connect_with_retry():
+    global client, db
+    while True:
+        try:
+            client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
+            client.admin.command("ping")  # Ping the MongoDB server
+            db = client[DB_NAME]
+            print("Connected to MongoDB")
+            break
+        except Exception as e:
+            print(f"Error connecting to MongoDB: {e}. Retrying in 5 seconds...")
+            time.sleep(5)
+
+
+connect_with_retry()
 
 
 def get_stats(db_session):
@@ -29,7 +51,7 @@ def get_stats(db_session):
 
     # Average passenger count
     cursor.execute(f"""
-                    select round(avg(passengers_onb), 0) from {CONFIG['mysql']['database']}.{CONFIG['mysql']['table']['name']}
+                    select round(avg(passengers_onb), 0) from {CONFIG["mysql"]["database"]}.{CONFIG["mysql"]["table"]["name"]}
                 """)
     results = cursor.fetchall()
     if results:
@@ -38,7 +60,7 @@ def get_stats(db_session):
 
     # Peak Traffic time - this is busiest hour
     cursor.execute(f"""
-                    select hour(arrival_time) as hour, count(*) as total from {CONFIG['mysql']['database']}.{CONFIG['mysql']['table']['name']}
+                    select hour(arrival_time) as hour, count(*) as total from {CONFIG["mysql"]["database"]}.{CONFIG["mysql"]["table"]["name"]}
                     group by hour(arrival_time)
                     order by total desc
                     limit 1;
@@ -50,7 +72,7 @@ def get_stats(db_session):
 
     # Most popular line
     cursor.execute(f"""
-                    select train_line, count(*) as total from {CONFIG['mysql']['database']}.{CONFIG['mysql']['table']['name']}
+                    select train_line, count(*) as total from {CONFIG["mysql"]["database"]}.{CONFIG["mysql"]["table"]["name"]}
                     group by train_line
                     order by total desc
                     limit 1;
@@ -60,19 +82,19 @@ def get_stats(db_session):
         print(f"Most popular line: {results[0][0]}")
         stats["most_popular_line"] = results[0][0]
 
-    # num cargo trains 
+    # num cargo trains
     # num passenger trains
     cursor.execute(f"""
-                    select train_type, count(*) from {CONFIG['mysql']['database']}.{CONFIG['mysql']['table']['name']}
+                    select train_type, count(*) from {CONFIG["mysql"]["database"]}.{CONFIG["mysql"]["table"]["name"]}
                     group by train_type;
                 """)
     results = cursor.fetchall()
     if results:
         for row in results:
-            if row[0] == 'passenger':
+            if row[0] == "passenger":
                 print(f"Num passenger trains: {row[1]}")
                 stats["num_passenger_trains"] = row[1]
-            elif row[0] == 'cargo':
+            elif row[0] == "cargo":
                 print(f"Num cargo trains: {row[1]}")
                 stats["num_cargo_trains"] = row[1]
 
@@ -80,6 +102,7 @@ def get_stats(db_session):
     db_session.close()
 
     return stats
+
 
 def send_to_mongo(stats):
     print(stats)
@@ -99,11 +122,13 @@ def send_to_mongo(stats):
     # # Close MongoDB connection
     # mongo_client.close()
 
+
 def main():
     # some kind of loop that keeps running
     # lowkey an infitie while loop would work i think
     stats = get_stats(establish_sql_connection())
     send_to_mongo(stats)
+
 
 if __name__ == "__main__":
     main()
